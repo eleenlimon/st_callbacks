6f5ac5e09ed33102632697223439351aa5725d3e
Eleen Limon <134656628+eleenlimon@users.noreply.github.com>
2023-09-05 11:34:06 -0700

    Create st_callbacks.py

---
+++ st_callbacks.py
@@ -0,0 +1,22 @@
+# CALLBACKS
+
+import streamlit as st
+
+# Convert kms to ms vice versa
+st.subheader('Distance converter')
+
+
+def miles_to_km():
+    st.session_state.km = st.session_state.miles * 1.609
+
+
+def km_to_miles():
+    st.session_state.miles = st.session_state.km * 0.621
+
+
+col1, buff, col2 = st.columns([2, 1, 2])
+with col1:
+    miles = st.number_input('Miles:', key='miles', on_change=miles_to_km)  # be able to return one or the other if different input from user
+
+with col2:
+    km = st.number_input('Km:', key='km', on_change=km_to_miles)
